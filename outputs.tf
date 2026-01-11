d82510f53c22a29e5ef66a7429312b762da397fa
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:23:27 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "web_pubsub_shared_private_link_resources" {
+  description = "All web_pubsub_shared_private_link_resource resources"
+  value       = azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources
+}
+
