5e6c3a09fff7a43d44a7ad740a69c53db257be64
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "web_pubsub_shared_private_link_resources" {
-  description = "All web_pubsub_shared_private_link_resource resources"
-  value       = azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources
-}
 output "web_pubsub_shared_private_link_resources_name" {
-  description = "List of name values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.name]
+  description = "Map of name values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.name }
 }
 output "web_pubsub_shared_private_link_resources_request_message" {
-  description = "List of request_message values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.request_message]
+  description = "Map of request_message values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.request_message }
 }
 output "web_pubsub_shared_private_link_resources_status" {
-  description = "List of status values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.status]
+  description = "Map of status values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.status }
 }
 output "web_pubsub_shared_private_link_resources_subresource_name" {
-  description = "List of subresource_name values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.subresource_name]
+  description = "Map of subresource_name values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.subresource_name }
 }
 output "web_pubsub_shared_private_link_resources_target_resource_id" {
-  description = "List of target_resource_id values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.target_resource_id]
+  description = "Map of target_resource_id values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.target_resource_id }
 }
 output "web_pubsub_shared_private_link_resources_web_pubsub_id" {
-  description = "List of web_pubsub_id values across all web_pubsub_shared_private_link_resources"
-  value       = [for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : v.web_pubsub_id]
+  description = "Map of web_pubsub_id values across all web_pubsub_shared_private_link_resources, keyed the same as var.web_pubsub_shared_private_link_resources"
+  value       = { for k, v in azurerm_web_pubsub_shared_private_link_resource.web_pubsub_shared_private_link_resources : k => v.web_pubsub_id }
 }
 
